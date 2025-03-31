595cad60edc1bafe6efc2f44a5fdcd555419bb84
ivandalechenko <ivandalechenko@gmail.com>
2025-03-31 23:56:11 +0300

    fix

--- src/components/Nav/NavBtn/NavBtn.jsx
+++ src/components/Nav/NavBtn/NavBtn.jsx
@@ -1,6 +1,16 @@
 import './NavBtn.scss';
-export default ({ text }) => {return (
-    <button className='NavBtn'>
-        {text}
-    </button>
-)}
+export default ({ text }) => {
+    return (
+        <div className='NavBtn'>
+            <div className='NavBtn_text'>
+                {text}
+            </div>
+            <div className='NavBtn_anim free_img'>
+                <div className='NavBtn_anim_black'></div>
+            </div>
+            <div className='NavBtn_anim free_img'>
+                <div className='NavBtn_anim_white'></div>
+            </div>
+        </div>
+    )
+}
