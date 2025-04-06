64d9850f76b05d830f5cb6d6bdbaf10f5e1816c3
ivandalechenko <ivandalechenko@gmail.com>
2025-04-06 17:10:18 +0300

    fux

--- src/components/About/AboutSliderItem/AboutSliderItem.jsx
+++ src/components/About/AboutSliderItem/AboutSliderItem.jsx
@@ -1,5 +1,5 @@
 import './AboutSliderItem.scss';
-export default ({ index, img, title, description, currentSlide }) => {
+export default ({ index, title, description, currentSlide }) => {
     return (
         <div className='AboutSliderItem_wrapper free_img' style={{
             // transition: `transform 500ms`,
@@ -10,7 +10,7 @@ export default ({ index, img, title, description, currentSlide }) => {
             <div className='AboutSliderItem '>
                 <div className='AboutSliderItem__img'>
                     <div className='AboutSliderItem__img_wrapper free_img'>
-                        <img src={img} alt="" className={`img ${index === 1 ? "img1" :
+                        <img src={`/img/dogs/${index}.webp`} alt="" className={`img ${index === 1 ? "img1" :
                             index === 2 ? "img2" :
                                 index === 3 ? "img3" :
                                     index === 4 ? "img4" : ""
@@ -18,7 +18,7 @@ export default ({ index, img, title, description, currentSlide }) => {
                     </div>
                     <div className='AboutSliderItem__title free_img'>
                         <div className='AboutSliderItem__title_index'>{index}</div>
-                        <div className='AboutSliderItem__title_title'>{title}</div>
+                        <div className='AboutSliderItem__title_title '>{title}</div>
                     </div>
                 </div>
                 <div className='AboutSliderItem__description'>
--- src/components/About/Slider/Slider.jsx
+++ src/components/About/Slider/Slider.jsx
@@ -63,7 +63,6 @@ export default () => {
                             key={index}
                             currentSlide={currentSlide}
                             index={index + 1}
-                            img={el.img}
                             title={el.title}
                             description={el.description}
                         />
