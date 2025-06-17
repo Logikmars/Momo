bb65d96cdd09a4d99ae5230093ae46f8edfe68ae
ivandalechenko <ivandalechenko@gmail.com>
2025-06-17 19:42:58 +0300

    fix

--- src/components/MediaLinks/MediaLinks.jsx
+++ src/components/MediaLinks/MediaLinks.jsx
@@ -4,13 +4,13 @@ export default ({ large }) => {
     return (
 
         <div className={`MediaLinks ${large && 'MediaLinks_large'}`}>
-            <a href="#">
+            <a href="https://x.com/PawseHQ">
                 <img src="/img/x.png" alt="" />
             </a>
-            <a href="#">
+            {/* <a href="#">
                 <img src="/img/tg.png" alt="" />
-            </a>
-            <a href="#">
+            </a> */}
+            <a href="https://dexscreener.com/solana/6xyymcm2vuffrqxkhxrtgpn6sdn3hkb4jdubevr1wq8f">
                 <img src="/img/dex.png" alt="" />
             </a>
         </div>
